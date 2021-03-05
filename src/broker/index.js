1ed0665064d66b57a3ae32fab8410f619c31ec28
Justin Bleile <justinbleile.dev@gmail.com>
2021-03-05 16:18:06 -0700

    Moving forward on logic for Broker

--- src/broker/index.js
+++ src/broker/index.js
@@ -11,7 +11,24 @@ class Broker {
     //Connects to authorized CoinbasePro account
     const authCRYPAccount = await AuthClient.getAccount(crypAccount);
     const authFIATAccount = await AuthClient.getAccount(fiatAccount);
+    const historicRatesController = new HistoricRates(curPair, rangeLength);
   }
+  async placeOrder(side, productId, price, side) {}
+
+  async getPreviousCandles(curPair, rangeLength, frequency) {
+    const candles = await historicRatesController.getHistoricRange(
+      curPair,
+      rangeLength,
+      frequency
+    );
+    return candles;
+  }
+
+  async getCurrentCandle(curPair, rangeLength, frequency) {}
+
+  async getNewCandle() {}
+
+  async updateStrategy() {}
 }
 
 module.exports = exports = Broker;
